3a5996794847e74e2cde1010321d7599c347c749
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-07-28 20:50:56 +0900

    Add error messages for 404 and 500 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception,                        with: :render_500
+  rescue_from ActiveRecord::RecordNotFound,     with: :render_404
+  rescue_from ActionController::RoutingError,   with: :render_404
+
+  def render_404
+    render json: { message: 'Not Found' }, status: 404
+  end
+
+  def render_500
+    render json: { message: 'Internal Server Error' }, status: 500
+  end
 end
